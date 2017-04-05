1a77f9fd6682cc3aec82ef3c0f43e6c1678fe5a5
Allen Fang <allenfang08@gmail.com>
2017-04-05 13:02:28 -0700

    first deploy

--- apps/houseplanner/models.py
+++ apps/houseplanner/models.py
@@ -6,6 +6,7 @@ from django.db import models
 import re
 import bcrypt
 import datetime
+from IPython import embed
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
@@ -60,8 +61,52 @@ class UserManager(models.Manager):
 		else:
 			return { 'error': errors }
 
+	def editinfo(self, postData):
+		errors = []
+		if len(postData['first_name']) < 2:
+			errors.append('First name must at least 2 letters')
+		if not postData['first_name'].isalpha():
+			errors.append('First name cannot contain numbers')
+		if len(postData['last_name']) < 2:
+			errors.append('Last name must at least 2 letters')
+		if postData['last_name'].isdigit():
+			errors.append('Last name cannot contain numbers')
+		if not EMAIL_REGEX.match(postData['email']):
+			errors.append('Invalid Email')
+		if not len(postData['phone']) == 10:
+			errors.append('Please enter a 10 digit phone number')
+		elif not postData['phone'].isdigit():
+			errors.append('Phone number must be digits')
+		if errors == []:
+			user =  postData['user']
+			user.first_name = postData['first_name']
+			user.last_name = postData['last_name']
+			user.email = postData['email']
+			user.phone = postData['phone']
+			user.save()
+			return True
+		else:
+			return {"error": errors}
+
 class ExpenseManager(models.Manager):
-	pass
+	def createExpense(self, postData):
+		errors = []
+		current_date = datetime.datetime.today().strftime('%Y-%m-%d')
+		if len(postData['expense_name']) < 1:
+		    errors.append("Expense name must not be blank!")
+		if len(postData['amount']) < 1:
+		    errors.append("Amount must not be blank!")
+		elif float(postData['amount']) <= 0:
+			errors.append("Amount can not be zero or a negative number!")
+
+		if not errors:
+			if postData['due_date'] == "":
+				expense = Expense.objects.create(name = postData['expense_name'], amount=float(postData['amount']), user=postData['user'])
+			else: 
+				expense = Expense.objects.create(name = postData['expense_name'], amount=float(postData['amount']), due_date = postData['due_date'], user=postData['user'])
+			return { 'expense': expense }
+		else:
+			return { 'error': errors }
 
 class CommentManager(models.Manager):
 	pass
@@ -70,7 +115,32 @@ class MessageManager(models.Manager):
 	pass
 
 class EventManager(models.Manager):
-	pass
+	def createEvent(self, postData):
+		errors = []
+		current_date = datetime.datetime.today().strftime('%Y-%m-%d')
+		if len(postData['title']) < 1:
+			errors.append('Event title cannot be blank!')
+		if postData['start'] == "":
+			errors.append('must have a start date')
+
+		if not errors:
+			if postData['startTime']:
+				start = postData['start'] + "T" + postData['startTime']
+			else:
+				start = postData['start']
+			if postData['endTime']:
+				end = postData['end'] + "T" + postData['endTime']
+			else:
+				end = postData['end']
+			if len(postData['end']) < 1:
+				end = postData['start']
+			# embed()
+			event = Event.objects.create(title=postData['title'], description=postData['description'], start=start, end=end, user=postData['user'], startDate=postData['start'])
+			return { 'event': event }
+		else:
+			return { 'error': errors }
+
+
 
 class House(models.Model):
 	nickname = models.CharField(max_length=255)
@@ -93,9 +163,8 @@ class User(models.Model):
 
 class Expense(models.Model):
 	name = models.CharField(max_length=255)
-	amount = models.IntegerField()
+	amount = models.FloatField()
 	due_date = models.DateField(null=True)
-	completed = models.IntegerField()
 	user = models.ForeignKey(User)
 	objects = ExpenseManager()
 	charges = models.ManyToManyField(User, related_name="charges")
@@ -118,10 +187,14 @@ class Comment(models.Model):
 	updated_at = models.DateTimeField(auto_now=True)
 
 class Event(models.Model):
-	name = models.CharField(max_length=255)
-	description = models.TextField()
-	date = models.DateField()
-	time = models.TimeField(blank=True)
+	title = models.CharField(max_length=255)
+	description = models.TextField(null=True)
+	start = models.CharField(max_length=255)
+	startDate = models.DateField()
+	startTime = models.TimeField(null=True)
+	end = models.CharField(max_length=255, null=True)
+	endDate = models.DateField(null=True)
+	endTime = models.TimeField(null=True)
 	user = models.ForeignKey(User)
 	objects = EventManager()
 	created_at = models.DateTimeField(auto_now_add=True)
--- apps/houseplanner/views.py
+++ apps/houseplanner/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect
-from .models import House, User, Expense, Message, Comment
+from django.shortcuts import render, redirect, HttpResponse
+from .models import House, User, Expense, Message, Comment, Event
 from IPython import embed
 from django.contrib import messages
 import random
 import string
 from IPython import embed
+from django.http import JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
+import json
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 # render home page
@@ -19,13 +23,62 @@ def login(request):
 
 # render dashboard
 def dashboard(request):
+	# Message.objects.all().delete()
+	# Comment.objects.all().delete()
 	user = User.objects.get(id=request.session['user'])
 	house = User.objects.get(id=request.session['user']).house
+	housemates = User.objects.filter(house=house)
+	messages= Message.objects.filter(user__house=house).order_by("-created_at")
+	# messages = Message.objects.all()
+	comments= Comment.objects.filter(user__house=house).order_by("-created_at")
+	events = Event.objects.filter(user__house=house).order_by("start")
 	context = {
 		'user': user,
-		'house': house
+		'house': house,
+		'housemates': housemates,
+		'messages': messages,
+		'comments': comments,
+		'events': events
 	}
 	return render(request, 'houseplanner/dashboard.html', context)
+
+############################################################################################################
+############################################################################################################
+############################################################################################################
+
+#render edit user page
+def edit_user(request):
+	user = User.objects.get(id=request.session['user'])
+	house = User.objects.get(id=request.session['user']).house
+	housemates = User.objects.filter(house=house)
+	context = {
+		'user': user,
+		'house': house,
+		'housemates': housemates
+	}
+	return render(request, 'houseplanner/edit_info.html', context)
+
+#process edits to user information
+def change_info(request):
+	postData = {
+		'user': User.objects.get(id=request.session['user']),
+		'first_name': request.POST['first_name'],
+		'last_name': request.POST['last_name'],
+		'email': request.POST['email'],
+		'phone': request.POST['phone']
+	}
+	user = User.objects.editinfo(postData)
+	if user == True:
+		messages.success(request, 'Your information has been updated')
+		return redirect('/dashboard')
+	else:
+		for message in user['error']:
+			messages.error(request, message)
+		return redirect('/edit_user')
+
+############################################################################################################
+############################################################################################################
+############################################################################################################
 
 # render charge interface
 def group_charge(request):
@@ -38,13 +91,6 @@ def group_charge(request):
 	}
 	return render(request, 'houseplanner/group_charge.html', context)
 
-# render calendar 
-def calendar(request):
-	user = User.objects.get(id=request.session['user'])
-	context = {
-		'user': user,
-	}
-	return render(request, 'houseplanner/calendar.html', context)
 
 # render joining or creating a house page after successful registration
 def join_create(request):
@@ -90,8 +136,12 @@ def process_logreg(request):
 			return redirect('/login')
 		if 'theuser' in user:
 			request.session['user'] = user['theuser'].id
-			return redirect('/dashboard')
+			if User.objects.get(id=request.session['user']).house == None:
+				return redirect('/join_create')
+			else:
+				return redirect('/dashboard')
 	return redirect('/dashboard')
+
 
 # creates a new house and adds user in making them admin
 def create_house(request, code):
@@ -113,15 +163,166 @@ def join_house(request):
 	else:
 		messages.error(request, "House does not exist!")
 		return redirect('/join_create')
-	
+
+
+
+############################################################################################################
+###################################MESSAGE BOARD############################################################
+############################################################################################################	
+
+# processes posting a message
+def post_message(request):
+	user = User.objects.get(id=request.session['user'])
+	house = User.objects.get(id=request.session['user']).house
+	message = Message.objects.filter(user__house = house).order_by('-created_at')
+	if request.POST['message_field']:
+		Message.objects.create(message=request.POST['message_field'], user=user)
+		context = {
+			'messages': message
+		}
+	else:
+		pass
+	return redirect('/dashboard')
+
+#processes posting a comment
+def post_comment(request, id):
+	user =  User.objects.get(id=request.session['user'])
+	message = Message.objects.get(id=id)
+	Comment.objects.create(comment=request.POST['comment_field'], user=user, message=message)
+	comment = Comment.objects.filter(message=message)
+	context = {
+		'comments' : comment
+	}
+	return render(request, 'houseplanner/comment.html', context)
+
+def view_message(request, id):
+	message = Message.objects.get(id=id)
+	comments = Comment.objects.filter(message=message)
+	context = {
+		'user': User.objects.get(id=request.session['user']),
+		'message': message,
+		'comments': comments
+	}
+	return render(request, 'houseplanner/message.html', context)
+############################################################################################################
+############################################################################################################
+############################################################################################################
+
+# renders expense history page
+def expense_history(request):
+	user = User.objects.get(id=request.session['user'])
+	house = user.house
+	expenses = Expense.objects.filter(user__house=house).order_by("-created_at")
+	context = {
+		'user': user,
+		'house': house,
+		'expenses': expenses
+	}
+	return render(request, 'houseplanner/expenses.html', context)
+
+
 # create transactions 
 def create_transactions(request):
-	print request.POST['expense_name']
-	print request.POST['amount']
-	print request.POST['due_date']
+	postData = {
+		'expense_name': request.POST['expense_name'],
+		'amount': request.POST['amount'],
+		'due_date': request.POST['due_date'],
+		'user': User.objects.get(id=request.session['user'])
+	}
+	expense = Expense.objects.createExpense(postData)
+	if 'expense' in expense:
+		return redirect('/expenses')
+	elif 'error' in expense:
+		for message in expense['error']:
+			messages.error(request, message)
+		return redirect('/group_charge')
+
+def delete_expense(request, id):
+	Expense.objects.get(id=id).delete()
+	return redirect('/expenses')
+
+# render calendar 
+def calendar(request):
+	# Event.objects.all().delete()
 	user = User.objects.get(id=request.session['user'])
-	embed()
-	return redirect('/charge')
+	context = {
+		'user': user,
+	}
+	return render(request, 'houseplanner/calendar.html', context)
+
+# grabs events from database, changes them to JSON data and sends it back to html
+# filter out only events made by users in the house
+def calendarEvents(request):
+	user = User.objects.get(id=request.session['user'])
+	house = user.house
+	housemates = User.objects.filter(house=house)
+	print housemates
+	events = []
+	for housemate in housemates:
+		if Event.objects.filter(user=housemate).exists():
+			user_events = Event.objects.filter(user=housemate);
+			for event in user_events:
+				events.append(event)
+	print events
+	eventObjects = []
+	for event in events:
+		eventObjects.append({'id':event.id, 'title': event.title, 'start': event.start, 'end': event.end, 'url': '/event/'+ str(event.id)})
+	return JsonResponse(eventObjects, safe=False)
+
+# add an event to the calendar
+def add_event(request):
+	if request.POST['end'] == "":
+		end = request.POST['start']
+	else:
+		end = request.POST['end']
+	print "end is" + end
+	postData = {
+		'title': request.POST['title'],
+		'start': request.POST['start'],
+		'end': end,
+		'startTime': request.POST.get('start_time', False),
+		'endTime': request.POST.get('end_time', False),
+		'description': request.POST['description'],
+		'user': User.objects.get(id=request.session['user'])
+	}
+	event = Event.objects.createEvent(postData)
+	if 'event' in event:
+		return redirect('/calendar')
+	elif 'error' in event:
+		for message in event['error']:
+			messages.error(request, message)
+		return redirect('/calendar')
+
+# update an event after dropping and releasing
+def event_info(request, id):
+	user = User.objects.get(id=request.session['user'])
+	event = Event.objects.get(id=id)
+	start_date = event.start[:10]
+	end_date = event.end[:10]
+	start_time = event.start[11:]
+	end_time = event.end[11:]
+	context = {
+		'user': user,
+		'event': event,
+		'start_date': start_date,
+		'end_date': end_date,
+		'start_time': start_time,
+		'end_time': end_time
+	}
+	return render(request, 'houseplanner/event.html', context)
+
+def delete_event(request, id):
+	Event.objects.get(id=id).delete()
+	return redirect('/calendar')
+
+def delete_message(request, id):
+	Message.objects.get(id=id).delete()
+	return redirect('/dashboard')
+
+def delete_comment(request, id):
+	Comment.objects.get(id=id).delete()
+	return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 # logs user out and redirects to login page
 def logout(request):
